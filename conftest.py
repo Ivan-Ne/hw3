9dc3d1f18096b9d217cff0e34bde4bc92bf13444
Ivan Neverov <neverov.iv@gmail.com>
2024-03-15 23:06:45 +0300

    Установил реальные размеры для браузера, убрал принты и комментарии, указал scope='function' в фикстуре

--- conftest.py
+++ conftest.py
@@ -1,13 +1,8 @@
 import pytest
 from selene import browser
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope='function')
 def browser_personal_settings():
     browser.config.base_url = 'https://google.com'
-    browser.config.window_height = 500
-    browser.config.window_width = 500
-    print('browser opened')
-
-    yield
-
-    print('browser closed')
+    browser.config.window_height = 1600
+    browser.config.window_width = 2560
